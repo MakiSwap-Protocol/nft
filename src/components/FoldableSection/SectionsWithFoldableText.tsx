e944485d5978daf28bfe9b337b3bdd4cd92725fe
sendevman <devman.sd@gmail.com>
2022-02-21 04:33:49 -0500

    faq block style

--- src/components/FoldableSection/SectionsWithFoldableText.tsx
+++ src/components/FoldableSection/SectionsWithFoldableText.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Text, Heading, Card, CardHeader, CardBody, Box, BoxProps } from 'maki-toolkit-v3'
 import { useTranslation } from 'contexts/Localization'
 import FoldableText from './FoldableText'
@@ -8,30 +9,42 @@ interface Props extends BoxProps {
   config: { title: string; description: string[] }[]
 }
 
+const StyledSectionsWithFoldableTextCard = styled.div`
+  width: calc(100% - 3px);
+  margin-top: 1px;
+  margin-left: 1px;
+  margin-bottom: 2px;
+  overflow: inherit;
+  background: #FFFFFF;
+  border-radius: 32px;
+`
+
 const SectionsWithFoldableText: React.FC<Props> = ({ header, config, ...props }) => {
   const { t } = useTranslation()
 
   return (
     <Box maxWidth="888px" {...props}>
       <Card>
-        <CardHeader>
-          <Heading scale="lg" color="secondary">
-            {header}
-          </Heading>
-        </CardHeader>
-        <CardBody>
-          {config.map(({ title, description }, i, { length }) => (
-            <FoldableText key={title} id={title} mb={i + 1 === length ? '' : '24px'} title={t(title)}>
-              {description.map((desc) => {
-                return (
-                  <Text key={desc} color="textSubtle" as="p">
-                    {t(desc)}
-                  </Text>
-                )
-              })}
-            </FoldableText>
-          ))}
-        </CardBody>
+        <StyledSectionsWithFoldableTextCard>
+          <CardHeader>
+            <Heading scale="lg" color="secondary">
+              {header}
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            {config.map(({ title, description }, i, { length }) => (
+              <FoldableText key={title} id={title} mb={i + 1 === length ? '' : '24px'} title={t(title)}>
+                {description.map((desc) => {
+                  return (
+                    <Text key={desc} color="textSubtle" as="p">
+                      {t(desc)}
+                    </Text>
+                  )
+                })}
+              </FoldableText>
+            ))}
+          </CardBody>
+        </StyledSectionsWithFoldableTextCard>
       </Card>
     </Box>
   )
